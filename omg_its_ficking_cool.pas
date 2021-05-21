3e35cebd21656d34fc08c15c562887f1a2822c58
Deyman Qr <61621130+DeymanQr@users.noreply.github.com>
2021-05-21 14:28:38 +0300

    Create omg_its_ficking_cool.pas

---
+++ omg_its_ficking_cool.pas
@@ -0,0 +1,46 @@
+uses crt, graph;
+const cell_size = 2; cell_count = 500; life_cell_color = 2; dead_cell_color = 8; x0 = 450; y0 = 10;
+var g, h, i, j, r: integer; p: boolean; a, b: array[1..cell_count, 1..cell_count] of byte;
+
+procedure paint_cell(x, y, c: integer);
+var i: integer;
+begin
+setcolor(c);
+for i := 1 to cell_size do line((x-1)*cell_size+1 + x0, (y-1)*cell_size + i + y0, x*cell_size + x0, (y-1)*cell_size + i + y0);
+end;
+
+function neighbors_sum(x, y: integer): byte;
+var s: byte;
+begin
+s := 0;
+if y > 1 then s := s + a[x, y-1];
+if x > 1 then s := s + a[x-1, y];
+if y < cell_count then s := s + a[x, y+1];
+if x < cell_count then s := s + a[x+1, y];
+if (x > 1) and (y > 1) then s := s + a[x-1, y-1];
+if (x < cell_count) and (y < cell_count) then s := s + a[x+1, y+1];
+if (x < cell_count) and (y > 1) then s := s + a[x+1, y-1];
+if (x > 1) and (y < cell_count) then s := s + a[x-1, y+1];
+neighbors_sum := s;
+end;
+
+begin
+g := detect; initgraph(g, h, '');
+p := true;
+randomize;
+for i := 1 to cell_count do for j := 1 to cell_count do begin
+r := random(20);
+if r = 0 then paint_cell(j, i, life_cell_color) else paint_cell(j, i, dead_cell_color);
+a[j, i] := ord(r = 0); b[j, i] := ord(r = 0);
+end;
+while p do begin
+p := false;
+for i := 1 to cell_count do for j := 1 to cell_count do begin
+r := neighbors_sum(j, i);
+if (r = 3) and (a[j, i] = 0) then begin b[j, i] := 1; paint_cell(j, i, life_cell_color); p := true; end
+else if ((r < 2) or (r > 3)) and (a[j, i] = 1) then begin b[j, i] := 0; paint_cell(j, i, dead_cell_color); p := true; end;
+end;
+for i := 1 to cell_count do for j := 1 to cell_count do a[j, i] := b[j, i];
+//delay(10);
+end;
+end.
